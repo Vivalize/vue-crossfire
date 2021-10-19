c8995377776bfdb9542d31895ba7798aeb61be66
Marko Ritachka <markomarkor@gmail.com>
2021-10-18 20:29:20 -0700

    Changed structure from a generic mixin to a Vue plugin

--- README.md
+++ README.md
@@ -3,7 +3,9 @@
 ![npm](https://img.shields.io/npm/v/vue-crossfire)
 ![npm bundle size](https://img.shields.io/bundlephobia/min/vue-crossfire)
 
-Simple two-way Firestore syncing in Vue
+Dead simple two-way Firestore syncing in Vue
+
+Works with projects built with Firebase V9 and Vue 2
 
 ## Install
 
@@ -11,7 +13,44 @@ Simple two-way Firestore syncing in Vue
 $ npm install vue-crossfire
 ```
 
-## Example Usage
+Then when initializing your Vue app:
+```vue
+import Crossfire from 'vue-crossfire'
+Vue.use(Crossfire)
+```
+
+## Usage
+
+#### $crossfire( firestoreReference[, options ] )
+
+This method returns the data at the respective Firestore reference, binded two-way. It will update reactively with any remote changes, and any local changes will be immedietly updated in Firestore. Returns **null** initially before the first update has been retrieved.
+
+```vue
+var documentSync = this.$crossfire(doc(db, 'docCollection', 'docID'))
+var collectionSync = this.$crossfire(collection(db, 'colletionName'), { readOnly: true })
+var querySync = this.$crossfire(query(collection(db, 'colletionName'), where('fieldID', '==', true)))
+```
+
+**firestoreReference** is a reference to a Firestore V9 document, collection, or query
+The method will return the data contained at that reference:
+  If reference is a document, it will be the document data
+  Otherwise, it will be an array of document data of the results
+  Before the first state has been downloaded, it will return null
+
+The optional **options** object can be configured with the following fields:
+* * *
+provideID: *Boolean - default: false*
+    If set to true, this method will return both the document data as well as the document ID(s)
+    in the form: { id: *docID*, data: *docData* } instead of only returning the document data.
+    In the case of collections or queries, it will return an array of objects in the above form.
+readOnly: *Boolean - default: false*
+    If true, don't update firestore if the local data is changed
+ignoreUnchangedFields: *Boolean - default: false*
+    When updating firestore docs, only update the specific fields that were changed in the data. This is slightly more computationally expensive, but is very useful when many users are editing a document at the same time and may overwrite data with their local state before they've received new changes
+transformUpdate: *function (changedData) - default undefined*
+    Whenever a document is to be updated in firestore, the data to be saved will instead use the result of this function (if it's provided). Useful if you want to attach update metadata to the document (such as "dateModified" or "lastModifiedBy")
+
+## Example
 
 ```vue
 <template>
@@ -23,16 +62,15 @@ $ npm install vue-crossfire
 </template>
 
 <script>
-  const cf = require('vue-crossfire')
+  import { doc } from 'firebase/firestore'
 
-  // Import crossfire into your mixins, pass a firestore reference into this.crossfire(),
+  // Pass a firestore reference into $crossfire(),
   // then simply read/write to the result as any regular object variable
   export default {
     name: 'VueComponent',
-    mixins: [cf],
     computed: {
       liveDoc: function () {
-        return this.crossfire(db.collection('someCollection').doc('docID'))
+        return this.$crossfire(doc(db, 'someCollection', 'docID'))
       },
     },
   }
--- index.js
+++ index.js
@@ -1,12 +1,39 @@
 import { updateDoc, onSnapshot } from 'firebase/firestore'
 
-export default {
-	data () {
-		return {
-			crossfireListeners: {},
+// Function that handles updating a doc that's changed
+function updateData (oldData, data, ref, options) {
+	if (options && options.readOnly) return
+	if (data === null) return
+	let update = JSON.parse(JSON.stringify(data))
+	if (options && options.ignoreUnchangedFields) {
+		const keys = Object.keys(update)
+		keys.forEach(k => {
+			if (objectsAreEqual(update[k], oldData[k])) delete update[k]
+		})
+	}
+	if (options && options.transformUpdate) update = options.transformUpdate(update)
+	return updateDoc(ref, update)
+}
+
+// Non-extensive object comparison function
+function objectsAreEqual (a, b) {
+	if ((a !== null && typeof a === 'object') && (b !== null && typeof b === 'object')) {
+		const keys = [...new Set([...Object.keys(a), ...Object.keys(b)])]
+		for (var i = 0; i < keys.length; i++) {
+			if (!objectsAreEqual(a[keys[i]], b[keys[i]])) return false
 		}
-	},
-	methods: {
+		return true
+	} else if ((a !== null && typeof a === 'object') || (b !== null && typeof b === 'object')) {
+		return false
+	} else {
+		return a === b
+	}
+}
+
+export default {
+	install (Vue, options) {
+		const crossfireListeners = Vue.observable({})
+
 		// Returns the data located in Firestore at the passed in "reference" parameter
 		// This method is meant to be used reactively in Vue, as the returned result will
 		// change based on listening state (will return null on first call, but will return
@@ -22,12 +49,12 @@ export default {
 		//		transformUpdate:
 		//			When a document is about to be updated in firestore because of
 		//			local changes, this function will be run on the data first
-		crossfire: function (reference, options) {
+		Vue.prototype.$crossfire = function (reference, options) {
 			if (!reference) return
 			const path = reference.path || reference._query.toString()
-			if (!this.crossfireListeners[path]) {
+			if (!crossfireListeners[path]) {
 				// Construct local data storage
-				this.$set(this.crossfireListeners, path, {
+				this.$set(crossfireListeners, path, {
 					doc: null,
 					data: null,
 					oldData: null,
@@ -45,35 +72,35 @@ export default {
 
 							// Setup single listener if reference is a document
 						if (reference.type === 'document') {
-							this.crossfireListeners[path].skipUpdate = true
-							this.crossfireListeners[path].doc = doc
-							this.crossfireListeners[path].data = doc.data()
-							this.crossfireListeners[path].oldData = JSON.parse(JSON.stringify(doc.data()))
+							crossfireListeners[path].skipUpdate = true
+							crossfireListeners[path].doc = doc
+							crossfireListeners[path].data = doc.data()
+							crossfireListeners[path].oldData = JSON.parse(JSON.stringify(doc.data()))
 						}
 						
 						// If reference is a query or collection, setup listeners for every document within
 						else {
-							this.crossfireListeners[path].queryDocs = doc.docs
+							crossfireListeners[path].queryDocs = doc.docs
 							doc.docs.forEach(d => {
-								if (!this.crossfireListeners[path].queryDocWatchers[d.id]) {
+								if (!crossfireListeners[path].queryDocWatchers[d.id]) {
 
 									// Create watchers for every doc
-									this.$set(this.crossfireListeners[path].queryDocSkipUpdate, d.id, true)
-									this.$set(this.crossfireListeners[path].queryDocWatchers, d.id, this.$watch(
-										function () { return this.crossfireListeners[path].queryData[d.id] },
+									this.$set(crossfireListeners[path].queryDocSkipUpdate, d.id, true)
+									this.$set(crossfireListeners[path].queryDocWatchers, d.id, this.$watch(
+										function () { return crossfireListeners[path].queryData[d.id] },
 										function (val) {
-											if (!this.crossfireListeners[path].queryDocSkipUpdate[d.id]) this.updateData(this.crossfireListeners[path].queryOldData[d.id], this.crossfireListeners[path].queryData[d.id], d.ref, options)
-											else this.crossfireListeners[path].queryDocSkipUpdate[d.id] = false
-											this.crossfireListeners[path].queryOldData[d.id] = JSON.parse(JSON.stringify(this.crossfireListeners[path].queryData[d.id]))
+											if (!crossfireListeners[path].queryDocSkipUpdate[d.id]) updateData(crossfireListeners[path].queryOldData[d.id], crossfireListeners[path].queryData[d.id], d.ref, options)
+											else crossfireListeners[path].queryDocSkipUpdate[d.id] = false
+											crossfireListeners[path].queryOldData[d.id] = JSON.parse(JSON.stringify(crossfireListeners[path].queryData[d.id]))
 										},
 										{ deep: true }
 									))
 								}
 
 								
-								this.crossfireListeners[path].queryDocSkipUpdate[d.id] = true
-								this.$set(this.crossfireListeners[path].queryData, d.id, d.data())
-								this.$set(this.crossfireListeners[path].queryOldData, d.id, JSON.parse(JSON.stringify(d.data())))
+								crossfireListeners[path].queryDocSkipUpdate[d.id] = true
+								this.$set(crossfireListeners[path].queryData, d.id, d.data())
+								this.$set(crossfireListeners[path].queryOldData, d.id, JSON.parse(JSON.stringify(d.data())))
 							})
 						}
 						}
@@ -81,59 +108,31 @@ export default {
 				})
 				
 				// Create watcher for document updates
-				this.$set(this.crossfireListeners[path], 'watcher', this.$watch(
-					function () { return this.crossfireListeners[path].data },
+				this.$set(crossfireListeners[path], 'watcher', this.$watch(
+					function () { return crossfireListeners[path].data },
 					function (val) {
-						if (!this.crossfireListeners[path].skipUpdate) this.updateData(this.crossfireListeners[path].oldData, this.crossfireListeners[path].data, this.crossfireListeners[path].ref, options)
-						else this.crossfireListeners[path].skipUpdate = false
-						this.crossfireListeners[path].oldData = JSON.parse(JSON.stringify(this.crossfireListeners[path].data))
+						if (!crossfireListeners[path].skipUpdate) updateData(crossfireListeners[path].oldData, crossfireListeners[path].data, crossfireListeners[path].ref, options)
+						else crossfireListeners[path].skipUpdate = false
+						crossfireListeners[path].oldData = JSON.parse(JSON.stringify(crossfireListeners[path].data))
 					},
 					{ deep: true }
 				))
 			}
 
 			// Return the firebase data associated with the passed reference
-			if (this.crossfireListeners[path].type === 'document') {
+			if (crossfireListeners[path].type === 'document') {
 				if (options && options.provideID) {
-					return { id: this.crossfireListeners[path].doc.id, data: this.crossfireListeners[path].data }
-				} else return this.crossfireListeners[path].data
+					return { id: crossfireListeners[path].doc.id, data: crossfireListeners[path].data }
+				} else return crossfireListeners[path].data
 			} else {
 				if (options && options.provideID) {
-					return this.crossfireListeners[path].queryDocs.map(d => {
-						return { id: d.id, data: this.crossfireListeners[path].queryData[d.id] }
+					return crossfireListeners[path].queryDocs.map(d => {
+						return { id: d.id, data: crossfireListeners[path].queryData[d.id] }
 					})
 				} else {
-					return this.crossfireListeners[path].queryDocs.map(d => this.crossfireListeners[path].queryData[d.id])
+					return crossfireListeners[path].queryDocs.map(d => crossfireListeners[path].queryData[d.id])
 				}
-			}
-		},
-		// Function that handles updating a doc that's changed
-		updateData: function (oldData, data, ref, options) {
-			if (options && options.readOnly) return
-			if (data === null) return
-			let update = JSON.parse(JSON.stringify(data))
-			if (options && options.ignoreUnchangedFields) {
-				const keys = Object.keys(update)
-				keys.forEach(k => {
-					if (this.objectsAreEqual(update[k], oldData[k])) delete update[k]
-				})
-			}
-			if (options && options.transformUpdate) update = options.transformUpdate(update)
-			return updateDoc(ref, update)
-		},
-		// Non-extensive object comparison function
-		objectsAreEqual(a, b) {
-			if ((a !== null && typeof a === 'object') && (b !== null && typeof b === 'object')) {
-				const keys = [...new Set([...Object.keys(a), ...Object.keys(b)])]
-				for (var i = 0; i < keys.length; i++) {
-					if (!this.objectsAreEqual(a[keys[i]], b[keys[i]])) return false
-				}
-				return true
-			} else if ((a !== null && typeof a === 'object') || (b !== null && typeof b === 'object')) {
-				return false
-			} else {
-				return a === b
 			}
 		}
-	}
+	},
 }
